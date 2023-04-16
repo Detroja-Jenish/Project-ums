c86cd216234310472b29e76ced5e38e4d9568616
Detroja Jenish <KrishaKalola@gmail.com>
2023-04-16 22:11:58 +0530

    finally addmisionForm done

--- app.py
+++ app.py
@@ -107,48 +107,48 @@ def registerStudent():
     photo       = request.files['photo'];
     photoName   = secure_filename(photo.filename);
     
-    #try:
-    connection  = mysql.connect();
-    cursor      = connection.cursor();
+    try:
+        connection  = mysql.connect();
+        cursor      = connection.cursor();
 
-    for number in contact:
-        cursor.execute(f"Insert into contactDetails (contactNo) values ('{number}')");
+        for number in contact:
+            cursor.execute(f"Insert into contactDetails (contactNo) values ('{number}')");
 
-    if photoName != None:
-        photoPath = 'static/students/'+photoName;
-        photo.save(photoPath);
-        cursor.execute(f"Insert into studentDetails (firstName, middleName, lastName, addharNo, gender, cityID, degreeID, courseID, photoPath) values ('{firstName}', '{middleName}','{lastName}', '{addharNo}', '{gender}', {cityID}, {degreeID}, {courseID}, '{photoPath}')");
-    else:
-        cursor.execute(f"Insert into studentDetails (firstName, middleName, lastName, addharNo, gender, cityID, degreeID, courseID) values ('{firstName}', '{middleName}','{lastName}', '{addharNo}', '{gender}', {cityID}, {degreeID}, {courseID})");
+        if photoName != None:
+            photoPath = 'static/students/'+photoName;
+            photo.save(photoPath);
+            cursor.execute(f"Insert into studentDetails (firstName, middleName, lastName, addharNo, gender, cityID, degreeID, courseID, photoPath) values ('{firstName}', '{middleName}','{lastName}', '{addharNo}', '{gender}', {cityID}, {degreeID}, {courseID}, '{photoPath}')");
+        else:
+            cursor.execute(f"Insert into studentDetails (firstName, middleName, lastName, addharNo, gender, cityID, degreeID, courseID) values ('{firstName}', '{middleName}','{lastName}', '{addharNo}', '{gender}', {cityID}, {degreeID}, {courseID})");
 
 
-    cursor.execute("select max(SID) from studentDetails");
-    SID = cursor.fetchall()[0][0];
+        cursor.execute("select max(SID) from studentDetails");
+        SID = cursor.fetchall()[0][0];
 
-    if(SID == None or SID == 0):
-        print(SID);
-        SID = 1;
+        if(SID == None or SID == 0):
+            print(SID);
+            SID = 1;
 
-    for contactNo in contact:
-        print(contactNo);
-        if contactNo != '' or contactNo != None:
-            cursor.execute(f"insert into studentWiseContact (contactId, SID) select contactID, {SID} from ContactDetails where ContactNo = '{contactNo}'");
+        for contactNo in contact:
+            print(contactNo);
+            if contactNo != '' or contactNo != None:
+                cursor.execute(f"insert into studentWiseContact (contactId, SID) select contactID, {SID} from ContactDetails where ContactNo = '{contactNo}'");
 
-    cursor.execute(f"Insert into addmisionDetails (SID, QutaId, courseId, date, year) values ({SID},{qutaID}, {courseID}, curdate(), year(curdate()))");
+        cursor.execute(f"Insert into addmisionDetails (SID, QutaId, courseId, date, year) values ({SID},{qutaID}, {courseID}, curdate(), year(curdate()))");
 
-    password = ''
-    for i in range(6):
-        password += random.choice(string.ascii_letters + string.digits);
+        password = ''
+        for i in range(6):
+            password += random.choice(string.ascii_letters + string.digits);
 
-    cursor.execute(f"Insert into loginPassword (userID, password, designation) values ('{contact_no1}', '{password}', 'student')")
-    cursor.execute(f"update studentDetails set userID = '{contact_no1}' where SID = {SID}")
-    connection.commit();
-    
-    #except Exception as e:
-    #    print("Exception : ", e);
+        cursor.execute(f"Insert into loginPassword (userID, password, designation) values ('{contact_no1}', '{password}', 'student')")
+        cursor.execute(f"update studentDetails set userID = '{contact_no1}' where SID = {SID}")
+        connection.commit();
+        
+    except Exception as e:
+        print("Exception : ", e);
 
-    #finally:
-    connection.close();
+    finally:
+        connection.close();
     
 
     return redirect("/addmisionForm");
